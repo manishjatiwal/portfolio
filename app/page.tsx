86fc08ebd5728de87cd252f66a7e778fb6b61ef4
Manish Jatiwal <manish.s.jatiwal@gmail.com>
2024-06-09 19:29:18 +0530

    fixed HTML entities error & img warning

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import Image from 'next/image'
 import Header from './components/header'
 
 export default function Home() {
@@ -7,11 +8,11 @@ export default function Home() {
       <Header />
       <section className='m-auto flex max-w-[1024px] flex-row justify-between'>
         <article>
-          Hi, I'm Manish Kumar. I'm a full stack developer. I love to build web
-          applications.
+          Hi, I&#39;m Manish Kumar. I&#39;m a full stack developer. I love to
+          build web applications.
         </article>
         <figure className='items-start rounded-full p-2'>
-          <img src='/manish.jpg' alt='Manish Kumar' className='' />
+          <Image src='/manish.jpg' alt='Manish Kumar' />
         </figure>
       </section>
     </main>
